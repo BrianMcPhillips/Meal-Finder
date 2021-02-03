9ad17499add32ec989b586ac19c73505c77e6e11
Brian McPhillips <B.McPhillips89@gmail.com>
2021-02-02 22:20:00 -0800

    add search meal function with fetch call to API

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const search = document.getElementById('search'),
   submit = document.getElementById('submit'),
   random = document.getElementById('random'),
   mealsElement = document.getElementById('meals'),
-  resultHeading = document.getElementById('results-heading'),
+  resultHeading = document.getElementById('result-heading'),
   singleMealElement = document.getElementById('single-meal');
 
 // Functions
@@ -15,7 +15,33 @@ function searchMeal(e) {
 
   singleMealElement.innerHTML = '';
   const term = search.value;
-  console.log(term);
+  
+  if(term.trim()) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        resultHeading.innerHTML = `<h2>Search results for "${term}" :</h2>`;
+
+        if(data.meals === null) {
+          resultHeading.innerHTML = `<p>There are no search results for ${term}.
+            Please Try Again.</p>`;
+        } else {
+          mealsElement.innerHTML = data.meals.map(meal => `
+            <div class="meals">
+              <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+              <div class="meal-info" data-mealID="${meal.idMeal}">
+                <h3>${meal.strMeal}</h3>
+              </div>
+            </div>`
+            )
+            .join('');
+        }
+      });
+    search.value = '';
+  } else {
+    alert('Please enter a search term')
+  }
 }
 
 // Event Listeners
